37f21e4aebd02a9183c04514a2c18c512e9c6349
sg <sgmheyhey@gmail.com>
2024-05-08 17:56:09 +0900

    변경사항 반영

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl springboot2orm | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -fa springboot2orm | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
